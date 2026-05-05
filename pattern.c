b5ffc7c11fbcf55e35fbcd4997e8fe7134a06794
Md Sakib <gameingsakib71@gmail.com>
2026-05-05 15:50:45 +0600

    patter

---
+++ pattern.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, star = 1;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) // printing n = line 
+    {
+        for(int j = 1; j <= star; j++) // printing * star 
+        {
+            printf("*");
+        }
+        star++;
+        printf("\n");
+    }
+    
+    return 0;
+}
+
+// input = n=5
+
+// output
+// *
+// **
+// ***
+// ****
+// *****
