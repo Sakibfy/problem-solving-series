ca72470a252f6d5c938a5d84597362a2b4d9f555
Md Sakib <gameingsakib71@gmail.com>
2026-05-07 11:36:28 +0600

    solving 1 - n number printing

---
+++ B_Print.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void num(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d", i);
+
+        if (i != n)
+        
+            printf(" ");
+        
+    }
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    num(n);
+    return 0;
+}
